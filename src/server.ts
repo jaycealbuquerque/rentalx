0d512711085ff50b35ed8508a5d567a8c99f06c0
jaycealbuquerque <jeycealbuquerquerocha@gmail.com>
2021-08-25 18:36:03 -0300

    Cria repositório de especificação

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRouters } from "./routes/specifications.routes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRouters);
 
 app.listen(3333, () => console.log("Server is running!"));
